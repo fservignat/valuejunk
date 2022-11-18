d76484099b6020593f805597462d1d71e6a58612
Francis Servignat <francis.servignat@outlook.com>
2022-11-18 16:15:37 +0100

    migration file for modif models

---
+++ db/migrate/20221118144422_add_title_address_to_junk.rb
@@ -0,0 +1,12 @@
+class AddTitleAddressToJunk < ActiveRecord::Migration[7.0]
+  def change
+    add_column :junks, :title, :string
+    add_column :junks, :address, :string
+    add_column :services, :title, :string
+    add_column :services, :address, :string
+    remove_column :users, :craft
+    remove_column :users, :specialty
+    rename_column :users, :description, :bio
+    rename_column :users, :pseudonym, :username
+  end
+end
