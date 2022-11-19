02ed4ec159e42c35a681e4012e8d6128084eb8f4
Sunny Law <sunny_law@hotmail.com>
2022-11-19 13:57:41 +0000

    add show page with link to index

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,8 +1,19 @@
 class ServicesController < ApplicationController
+  before_action :set_service, only: [:show]
 
   def index
     @services = Service.all
   end
+
+  def show
+
+  end
+
+  private
+
+    def set_service
+      @service = Service.find(params[:id])
+    end
 
 
 end
