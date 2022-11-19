25ea51532e61f3c4ab9cc908357d34b935d20a35
Kylian Janssens <kylianjanssens@hotmail.com>
2022-11-19 15:55:47 +0100

    new and create is working

--- app/controllers/junks_controller.rb
+++ app/controllers/junks_controller.rb
@@ -6,4 +6,24 @@ class JunksController < ApplicationController
   def show
     @junk = Junk.find(params[:id])
   end
+
+  def new
+    @junk = Junk.new
+  end
+
+  def create
+    @junk = Junk.new(junk_params)
+    @junk.user = current_user
+    if @junk.save
+      redirect_to junk_path(@junk)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def junk_params
+    params.require(:junk).permit(:title, :address, :description, :price, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :junks, only: [:index, :show]
+  resources :junks, only: [:index, :show, :new, :create]
 
   resources :services
 end
