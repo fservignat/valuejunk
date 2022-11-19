612be204b4cb24110f64c921b13d2f711058b14e
Kylian Janssens <kylianjanssens@hotmail.com>
2022-11-19 16:18:43 +0100

    We can edit a junk item

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :junks, only: [:index, :show, :new, :create]
+  resources :junks, only: [:index, :show, :new, :create, :edit, :update ]
 
   resources :services, only: [:index, :show]
 
