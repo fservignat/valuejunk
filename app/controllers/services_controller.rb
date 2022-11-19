a46d960c40f6fc338c168ded032aa9fdb685a52f
Sunny Law <sunny_law@hotmail.com>
2022-11-19 14:28:06 +0000

    add the new and create methods

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,5 @@
 class ServicesController < ApplicationController
-  before_action :set_service, only: [:show]
+  before_action :set_service, only: [:show, :create]
 
   def index
     @services = Service.all
@@ -9,11 +9,28 @@ class ServicesController < ApplicationController
 
   end
 
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
     def set_service
       @service = Service.find(params[:id])
     end
 
+    def service_params
+      params.require(:service).permit(:description, :price, :craft, :volunteer, :title, :address, :user_id)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   resources :junks, only: [:index, :show]
 
-  resources :services, only: [:index, :show]
+  resources :services
 
 end
