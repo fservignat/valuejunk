8230e6cf3499480ab59a93d84709b57ce6d3ed25
Francis Servignat <francis.servignat@outlook.com>
2022-11-22 20:33:02 +0100

    added several imgs in seeds and have working junk card but just way too big for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,8 +55,10 @@ junk = Junk.new(
   category:"Materials",
   price: 0,
   user: k_user)
-  # file = URI.open('https://res.cloudinary.com/dfkgih0bp/image/upload/v1668158134/development/Experimental_Cocktail_Club_gzeegk.png')
-  # junk.photos.attach(io: file, filename: 'experimental.jpg', content_type: 'image/jpg')
+  file = URI.open('https://images.unsplash.com/photo-1519668106955-a24e5f67f8c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8anVua3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60')
+  junk.photos.attach(io: file, filename: 'experimental.jpg', content_type: 'image/jpg')
+  file2 = URI.open('https://images.unsplash.com/photo-1519668106955-a24e5f67f8c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8anVua3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60')
+  junk.photos.attach(io: file2, filename: 'experimental.jpg', content_type: 'image/jpg')
 
 junk.save!
 puts "junk: #{junk.title} created."
@@ -85,7 +87,11 @@ junk = Junk.new(
   price: 0,
   user: k_user)
   file = URI.open('https://res.cloudinary.com/dfkgih0bp/image/upload/v1668940532/development/ValueJunk/internal%20img%20%28website%20itself%29/images/yarn-balls_caseiq.jpg')
+  file2 = URI.open('https://res.cloudinary.com/dfkgih0bp/image/upload/v1668940532/development/ValueJunk/internal%20img%20%28website%20itself%29/images/yarn-balls_caseiq.jpg')
+  file3 = URI.open('https://res.cloudinary.com/dfkgih0bp/image/upload/v1668940532/development/ValueJunk/internal%20img%20%28website%20itself%29/images/yarn-balls_caseiq.jpg')
   junk.photos.attach(io: file, filename: 'experimental.jpg', content_type: 'image/jpg')
+  junk.photos.attach(io: file2, filename: 'experimental.jpg', content_type: 'image/jpg')
+  junk.photos.attach(io: file3, filename: 'experimental.jpg', content_type: 'image/jpg')
 
 junk.save!
 puts "junk: #{junk.title} created."
