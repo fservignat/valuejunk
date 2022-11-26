046c9e289deef1d14b939d6d0c4dd4f5646eac3c
Sunny Law <sunny_law@hotmail.com>
2022-11-26 22:10:15 +0000

    reformat the input and add selection to craft

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,13 +1,18 @@
 class Service < ApplicationRecord
+
   belongs_to :user
 
   has_many_attached :photos
 
   has_many :specialties, through: :service_specialties
 
+  $craft = ["","Ceramics and glass", "Fibre and textile", "Flower", "Leatherwork", "Houseware", "Fashion", "Needlework", "Paper", "Wood and furniture", "Stone", "Metal"]
+
   validates :title, presence: true
   validates :address, presence: true
   validates :description, length: { in: 10..400 }
   validates :craft, presence: true
-  validates :craft, inclusion: { in: ["Ceramics and glass", "Fibre and textile", "Flower", "Leatherwork", "Houseware", "Fashion", "Needlework", "Paper", "Wood and furniture", "Stone", "Metal"] }
+  validates :craft, inclusion: { in: $craft }
+
+
 end
