e04c2032e9bcc53e48440236ddf4478e1c0247ff
Kylian Janssens <kylianjanssens@hotmail.com>
2022-11-26 12:56:15 +0100

    map customized and map in show page from junk

--- app/controllers/junks_controller.rb
+++ app/controllers/junks_controller.rb
@@ -1,7 +1,7 @@
 class JunksController < ApplicationController
   def index
     @junks = Junk.all
-    # The `geocoded` scope filters only junks with coordinates
+      # The `geocoded` scope filters only junks with coordinates
     @markers = @junks.geocoded.map do |junk|
       {
         lat: junk.latitude,
@@ -9,46 +9,48 @@ class JunksController < ApplicationController
         info_window: render_to_string(partial: "popup_map", locals: { junk: junk }),
         image_url: helpers.asset_url("hammer-solid.svg")
       }
+      end
     end
-  end
 
   def show
     @junk = Junk.find(params[:id])
-  end
+    @markers = [{lat: @junk.latitude, lng: @junk.longitude, image_url: helpers.asset_url("hammer-solid.svg")}]
 
-  def new
-    @junk = Junk.new
-  end
+    end
 
-  def create
-    @junk = Junk.new(junk_params)
-    @junk.user = current_user
-    if @junk.save
+    def new
+      @junk = Junk.new
+    end
+
+    def create
+      @junk = Junk.new(junk_params)
+      @junk.user = current_user
+      if @junk.save
+        redirect_to junk_path(@junk)
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @junk = Junk.find(params[:id])
+    end
+
+    def update
+      @junk = Junk.find(params[:id])
+      @junk.update(junk_params)
       redirect_to junk_path(@junk)
-    else
-      render :new
+    end
+
+    def destroy
+      @junk = Junk.find(params[:id])
+      @junk.destroy
+      redirect_to junks_path, status: :see_other
+    end
+
+    private
+
+    def junk_params
+      params.require(:junk).permit(:title, :address, :description, :price, :category, photos: [])
     end
   end
-
-  def edit
-    @junk = Junk.find(params[:id])
-  end
-
-  def update
-    @junk = Junk.find(params[:id])
-    @junk.update(junk_params)
-    redirect_to junk_path(@junk)
-  end
-
-  def destroy
-    @junk = Junk.find(params[:id])
-    @junk.destroy
-    redirect_to junks_path, status: :see_other
-  end
-
-  private
-
-  def junk_params
-    params.require(:junk).permit(:title, :address, :description, :price, :category, photos: [])
-  end
-end
