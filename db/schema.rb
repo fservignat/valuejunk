33511f6dd665537a7091603381fafb3c5ff502c2
Sunny Law <sunny_law@hotmail.com>
2022-11-28 21:21:20 +0000

    merging content

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_11_27_135447) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_11_26_135652) do
->>>>>>> 641de06f02bb80d6fca640ab676635ae6433e134
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
